931e147d7b9da993c1728e0b361eda186ab05eaa
kenti <016mki69@gmail.com>
2024-11-19 22:11:41 -0300

    Feat: add Email service with sendEmail method

--- demo/src/main/java/com/spring_security/jwt_auth/demo/DemoApplication.java
+++ demo/src/main/java/com/spring_security/jwt_auth/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.spring_security.jwt_auth.demo;
 
+import com.spring_security.jwt_auth.demo.service.EmailService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(EmailService emailService){
+		return args -> {
+			emailService.sendEmail("mopup2017@gmail.com", "Greeting", "Hello World!");
+		};
 	}
 
 }
